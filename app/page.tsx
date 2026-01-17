df22110408e1ac5f617f4989b1d9a46b5883bc4c
Rahul A Mistry <rahulmistry.sde@gmail.com>
2026-01-17 20:13:40 +0530

    chore: remove example codebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import { ComponentExample } from "@/components/component-example";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button className="cursor-pointer">
+        Trueloggs
+      </Button>
+    </div>
+  )
 }
